b4289643b97e66c45da526a27f3f468fa62509ae
glenn <glenn@mytopfun.com>
2020-06-28 18:37:21 +0800

    selection sort

--- sort/selection_sort.py
+++ sort/selection_sort.py
@@ -4,8 +4,21 @@
 import numpy as np
 
 def selection_sort(lst):
-    pass
-
+    n = len(lst)
+    for i in range(n):
+        # 默认未排序区间第一个元素最小
+        # min记录最小元素的index
+        min = i
+        # 然后从下一位元素开始遍历
+        # 寻找最小值的index
+        for j in range(i+1,n):
+            if lst[j] < lst[min]:
+                min = j
+        # 如果min有改变，说明找到了更小的值
+        # 交换原最小值和新最小值的位置
+        if min != i:
+            lst[i],lst[min] = lst[min],lst[i]
+    return lst
 
 lst = [3,5,4,1,2,6]
 print(lst)
